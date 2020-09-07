48c9a178597e7100449e7cfb5b212afea8dd6369
Evgeniy Grigorev <timeinvest27@gmail.com>
2020-09-07 13:05:26 +0300

    Список номеров будет храниться одной строкой

--- Parser/Models/Owner.cs
+++ Parser/Models/Owner.cs
@@ -8,7 +8,7 @@ namespace Parser.Models
         public Guid Id { get; set; }
         public string Url { get; set; }
         public string Name { get; set; }
-        public List<string> PhoneNumbers { get; set; } = new List<string>();
+        public string PhoneNumbers { get; set; }
         public string Description { get; set; }
         public string Site { get; set; }
         public string OtherInfo { get; set; }
