fdf0e7ab9a699184cab62520724354524a11183a
Evgeniy Grigorev <timeinvest27@gmail.com>
2020-09-08 13:07:52 +0300

    Добавление NLog и NLog.Config

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -1,4 +1,5 @@
-﻿using Parser.Cian;
+﻿using NLog;
+using Parser.Cian;
 using Parser.DtoModels;
 using System;
 using System.IO;
@@ -7,12 +8,13 @@ namespace Parser
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
             CianParser parser = new CianParser();
 
             parser.StartAsync("https://kazan.cian.ru/cat.php?currency=2&deal_type=sale&engine_version=2&foot_min=10&maxprice=5000000&offer_type=flat&only_foot=2&region=4777&room3=1&room4=1");
-
 
             Scheduler.Start();
             Console.WriteLine("Hello World!");
